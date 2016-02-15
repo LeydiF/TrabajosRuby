2ecb4bef782b6f1e3ae7a443fead925ae0ad65d0
Leydi <leidyflor2007@hotmail.com>
2016-02-15 15:03:31 -0500

    Segundo ejercicio listo

---
+++ ejercicio02.rb
@@ -0,0 +1,45 @@
+print "Seleccione una operacion:
+1. suma
+2. resta
+3. multiplicacion
+4. division"
+operacion = gets.chomp
+
+case operacion
+	when "1"
+		puts "digite el primer numero"
+		numero1 = gets.chomp.to_f
+		puts "digite el segundo numero"
+		numero2 = gets.chomp.to_f
+		resultado = numero1 + numero2
+		puts "este es el resultado de su operacion: #{resultado}"
+		
+	when "2"
+		puts "digite el primer numero"
+		numero1 = gets.chomp.to_f
+		puts "digite el segundo numero"
+		numero2 = gets.chomp.to_f
+		resultado = numero1 - numero2
+		puts "este es el resultado de su operacion: #{resultado}"
+		
+	when "3"
+		puts "digite el primer numero"
+		numero1 = gets.chomp.to_f
+		puts "digite el segundo numero"
+		numero2 = gets.chomp.to_f
+		resultado = numero1 * numero2
+		puts "este es el resultado de su operacion: #{resultado}"
+		
+	when "4"
+		puts "digite el primer numero"
+		numero1 = gets.chomp.to_f
+		puts "digite el segundo numero"
+		numero2 = gets.chomp.to_f
+		resultado = numero1 / numero2
+		puts "este es el resultado de su operacion: #{resultado}"
+	else 
+		puts "Seleccione una operacion"
+	end
+
+
+		
